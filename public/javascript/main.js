d243789b3e1969b4943b519b6d5151370c70d2bc
txreplay <txreplay@gmail.com>
2019-06-24 16:28:04 +0200

    anchor for scroll up/down

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,7 +1,21 @@
 (function() {
-    console.log('aaa');
     new fullpage('#fullpage', {
         lazyLoading: true,
         dragAndMove: true
     });
+
+    const $arrowContents = document.getElementsByClassName('arrow-content');
+    for (let i = 0; i < $arrowContents.length; i++) {
+        const elem = $arrowContents[i];
+        elem.onclick = () => {
+            const direction = elem.classList[1];
+            if (direction === 'down') {
+                fullpage_api.moveSectionDown();
+            }
+            if (direction === 'up') {
+                fullpage_api.moveSectionUp();
+            }
+            return false;
+        }
+    }
 })();
